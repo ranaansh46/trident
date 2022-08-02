4ad6106973392ead1c98c003742c3dd550805724
ranaansh46 <ranaansh46@gmail.com>
2022-08-02 18:09:23 +0530

    code formatted

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 from rich.prompt import Prompt
 from rich import print
 import typer
+
+#variables
 key = "key"
 username = "username"
 pin = 1234
-username = Prompt.ask("[bold white]Enter username")
-pin = int(Prompt.ask("[bold white]Enter Pin"))
-key = username + str(pin)
-print(key)
-temp_key = ""
-for i in key:
-    temp_key = temp_key + str(ord(i))
 
-key = temp_key
-print(key)
+#Functions
+def keymake():
+    key = username + str(pin)
+    print(key)
+    temp_key = ""
+    for i in key:
+        temp_key = temp_key + str(ord(i))
+
+    key = temp_key
+    key = bin(int(key))
+    print(key)
+
+if __name__=="main":
+    username = Prompt.ask("[bold white]Enter username")
+    pin = int(Prompt.ask("[bold white]Enter Pin"))
+    keymake()
