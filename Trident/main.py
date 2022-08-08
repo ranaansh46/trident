e2cae608a9bf1a78b02339c8d582efd38175993b
ranaansh46 <ranaansh46@gmail.com>
2022-08-08 18:13:19 +0530

    added show password

--- Trident/main.py
+++ Trident/main.py
@@ -5,8 +5,8 @@ from rich.panel import Panel
 import rich,os
 from crypto import cryptograph 
 from Models import models
-
-
+from rich.table import Table
+from rich.console import Console
 
 def signup():
     ch=1
@@ -37,30 +37,50 @@ def signin():
             else:
                 ch=1
 
-def addpasswd(sno,username,pin,db):
-    sno = sno + 1
+def addpasswd(username,pin,db,check:bool=True):
+    if check:
+        __sno = db.returnsno()
+        rich.print(__sno)
+    else:
+        __sno = 0
+    _sno = __sno + 1
     website = Prompt.ask("Enter website name")
     password = Prompt.ask("Enter password")
     _init_crypto = cryptograph.Cryptograph(username,pin)
     _key = _init_crypto.makekey()
     _xor_passwd= _init_crypto.xorthese(password,_key)
-    db.intovalue(sno,website,_xor_passwd)
+    db.intovalue(_sno,website,_xor_passwd)
+
+def showpasswd(db,username,pin):
+    _init_crypto = cryptograph.Cryptograph(username, pin)
+    table = Table(title="List of your password")
+    table.add_column("S.no",style="cyan")
+    table.add_column("Website",style="magenta")
+    table.add_column("Passwords",style="green")
+    i = db.returnsno()
+    for x in range(i):
+        s,w,p = db.returnvalue(x)
+        _p = _init_crypto.binxor(p,_init_crypto.makekey(username,pin))
+        _p = _init_crypto.charthis(_p)
+        table.add_row(s,w,_p)
+        console = Console()
+        console.print(table)
+        
 
 
 if __name__ == "__main__":
     rich.print(Panel("[bold white]Welcome to Trident Password Manager",subtitle="TPM version 0.0.1"))
     choice = Prompt.ask("Choose Signup : u or Signin : i",choices=["u","i"], default="u")
-    sno = 0
     if choice == "u": #this is sign up section
         db,username,pin = signup()
         sno = 0
         ch = 1
+        db.createtable()
+        addpasswd(username,pin,db,check=False)
         while ch!=0:
-            db.createtable()
-            addpasswd(sno,username,pin,db)
             choice = Prompt.ask("add more : a or Save and exit : s",choices=['a','s'],default='s')
             if choice == 'a':
-                pass
+                addpasswd(username, pin, db)
             else:
                 ch=0
 
@@ -69,19 +89,11 @@ if __name__ == "__main__":
         while ch!=0:
                 db,username,pin = signin()
                 ech=1
-                sno = db.returnsno()
-                rich.print(sno)
                 while ech!=0:
-                    addpasswd(sno,username,pin,db)
+                    addpasswd(username,pin,db)
                     choice = Prompt.ask("add more : a or Save and exit : s",choices=['a','s'],default='s')
                     if choice == 'a':
                         pass
                     else:
                         ech=0    
                 ch=0
-        
-
-    #  finalchar=_init_crypto.binxor(_key,_xor_passwd)
-    #         print(finalchar)
-    #         paswd = _init_crypto.charthis(finalchar)
-    #         print(paswd)
